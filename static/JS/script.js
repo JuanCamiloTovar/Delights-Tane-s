2606523526ad9bddb9b381c6c2e4aaecfba2d6fe
Juan Camilo Tovar <tovarcortesjuancamilo29@gmail.com>
2025-04-24 10:28:09 -0500

    first commit

---
+++ static/JS/script.js
@@ -0,0 +1,12 @@
+// FUNCION PARA REDIRECCIONAR AL HOME DE LA PAGINA
+function home(){
+    location.href = "index.html"
+}
+
+
+
+// FUNCION PARA DIRECIONAR A LA INFORMACION DEL POSTRE NAPOLEON
+
+function Information_napoleon() {
+    location.href="info_napoleon.html"
+}
